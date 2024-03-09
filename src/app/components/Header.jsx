4f49d9a0c41df2e47ec8f86f59711ae83e7017a1
iamrajeev2005 <fea.rajeev@gmail.com>
2024-03-09 21:51:21 +0530

    Header component complete

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+
+function Header() {
+  return (
+    <div className="w-screen h-20 bg-white flex items-center justify-between px-12">
+      <div className="logo w-28 ">
+        <img
+          src="https://app.koinx.com/static/media/Logo.3331aa2fc2f35c00e58921b44a2ebf0d.svg"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-links flex items-center gap-8">
+        {["Crypto Taxes", "Free Tools", "Resource Center"].map(
+          (item, index) => (
+            <Link key={index} className="font-bold text-zinc-900" href={"/#"}>
+              {item}
+            </Link>
+          )
+        )}
+        <div className="rounded-md bg-blue-600 text-white px-6 py-2">
+          <button>Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
+import Header from "./components/Header";
 
 export default function Home() {
-  return <>
-  </> 
+  return <div className="bg-zinc-200 min-h-screen w-screen">
+    <Header/>
+  </div>;
 }
