c8dda8fe7c1417bbd0acc3fc27496d0c259d7592
iamrajeev2005 <fea.rajeev@gmail.com>
2024-03-11 10:54:17 +0530

    overview tab responsive for medium devices done

--- src/app/components/Overview.jsx
+++ src/app/components/Overview.jsx
@@ -12,12 +12,12 @@ export default function Component() {
           </div>
           <div className="relative">
             <div className="mx-24 h-1 rounded-full bg-gradient-to-r from-red-500 to-green-500"></div>
-            <div className="absolute left-[70%] top-2">
+            <div className="absolute left-[60%] top-2">
               <div
                 className="w-0 h-0 
-  border-l-[5px] border-l-transparent
-  border-b-[10px] border-b-black
-  border-r-[5px] border-r-transparent"
+                          border-l-[5px] border-l-transparent
+                          border-b-[10px] border-b-black
+                          border-r-[5px] border-r-transparent"
               />
               <span className="absolute -translate-x-[50%] top-4 text-sm text-zinc-900">
                 $48,637.83
@@ -48,7 +48,7 @@ export default function Component() {
           Fundamentals
           <InfoIcon className="ml-2 w-5 h-5 text-gray-400" />
         </h2>
-        <div className="grid grid-cols-2 gap-8 mt-4">
+        <div className="grid grid-cols-2 max-md:flex max-md:flex-col gap-8 mt-4">
           <div>
             <div className="flex justify-between border-b-[1px] border-zinc-300 pb-3">
               <span className=" text-sm text-zinc-500">Bitcoin Price</span>
@@ -66,12 +66,12 @@ export default function Component() {
               <span className=" text-sm text-zinc-500">Trading Volume</span>
               <span className="font-bold text-sm">$23,249,202,782</span>
             </div>
-            <div className="flex justify-between mt-2 border-b-[1px] border-zinc-300 pb-3">
+            <div className="flex justify-between mt-2 border-b-[1px] border-zinc-300 pb-3 ">
               <span className=" text-sm text-zinc-500">Market Cap Rank</span>
               <span className="font-bold text-sm">#1</span>
             </div>
           </div>
-          <div>
+          <div className=" max-md:-mt-3">
             <div className="flex justify-between border-b-[1px] border-zinc-300 pb-3">
               <span className=" text-sm text-zinc-500">Market Cap</span>
               <span className="font-bold text-sm">$323,507,290,047</span>
@@ -92,13 +92,13 @@ export default function Component() {
               <span className=" text-sm text-zinc-500">All-Time High</span>
               <div className="flex flex-col items-end leading-tight">
                 <div>
-                  <span className="font-semibold text-[1vw]">
+                  <span className="font-semibold text-[1vw] max-md:text-sm">
                     $69,044.77
                     <span className="text-red-500"> -75.6%</span>
                   </span>
                 </div>
                 <div>
-                  <span className="text-[1vw] text-zinc-700">
+                  <span className="text-[1vw] max-md:text-sm text-zinc-700">
                     Nov 10, 2021 (about 1 year)
                   </span>
                 </div>
@@ -108,13 +108,13 @@ export default function Component() {
               <span className=" text-sm text-zinc-500">All-Time Low</span>
               <div className="flex flex-col items-end leading-tight">
                 <div>
-                  <span className="font-semibold text-[1vw]">
+                  <span className="font-semibold text-[1vw] max-md:text-sm">
                     $67.81
                     <span className="text-green-500"> 24729.1%</span>
                   </span>
                 </div>
                 <div>
-                  <span className="text-[1vw] text-zinc-700">
+                  <span className="text-[1vw] max-md:text-sm text-zinc-700">
                     Jul 6, 2013 (over 9 year)
                   </span>
                 </div>
